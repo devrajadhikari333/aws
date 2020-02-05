d377bc488b22245175ee3772c4993930f1f3a369
Devraj Adhikari <devrajadhikari333@gmail.com>
2020-02-05 18:29:43 +0000

    added one more variables

--- arthematic.sh
+++ arthematic.sh
@@ -2,6 +2,7 @@
 clear
 read -p "please enter the first number: " fnum
 read -p "Please enter the second number: " snum
+read -p "Please enter the third number: " tnum
 
 sum=$((fnum+snum))
 echo "sum is $sum"
